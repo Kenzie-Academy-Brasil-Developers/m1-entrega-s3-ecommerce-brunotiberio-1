472625da68ed35e0e67480ff7f6c33aeb50b3512
Bruno Tiberio <quentemente@gmail.com>
2022-03-18 14:01:32 -0300

    arrumar a pesquisa

---
+++ assets/js/pesquisa.js
@@ -0,0 +1,12 @@
+function encontrarProdutos(){
+    const valorInput = document.getElementById('inputPesquisa');
+    const novoData = [];
+
+    for (let i = 0; i < produtos.length; i++){
+        if(produtos[i].tags.indexOf(valorInput.value) !== -1){
+            novoData.push(produtos[i])
+        }
+    }
+    filtroTags(novoData);
+}
+
